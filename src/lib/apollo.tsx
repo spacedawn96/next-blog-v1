227a69f6b43845257fb8836b7528981001b6ba5a
spacedawn96 <spacedawn96@gmail.com>
2020-08-28 04:32:44 +0900

    update apollo

--- src/lib/apollo.tsx
+++ src/lib/apollo.tsx
@@ -1,15 +1,72 @@
 import { useMemo } from 'react';
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
-
+import { TokenRefreshLink } from "apollo-link-token-refresh";
+import { getAccessToken, setAccessToken } from './accessToken';
+import jwtDecode from "jwt-decode";
+import { setContext } from "apollo-link-context";
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
 let apolloClient;
 
 function createApolloClient() {
+  const httpLink = new HttpLink({
+    uri: "https://blog-server-v1.herokuapp.com/graphql",
+    credentials: "include",
+    fetch
+  });
+
+  const refreshLink = new TokenRefreshLink({
+    accessTokenField: "accessToken",
+    isTokenValidOrUndefined: () => {
+      const token = getAccessToken();
+
+      if (!token) {
+        return true;
+      }
+
+      try {
+        const { exp } = jwtDecode(token);
+        if (Date.now() >= exp * 1000) {
+          return false;
+        } else {
+          return true;
+        }
+      } catch {
+        return false;
+      }
+    },
+    fetchAccessToken: () => {
+      return fetch("https://blog-server-v1.herokuapp.com/refresh_token", {
+        method: "POST",
+        credentials: "include"
+      });
+    },
+    handleFetch: accessToken => {
+      setAccessToken(accessToken);
+    },
+    handleError: err => {
+      console.warn("Your refresh token is invalid. Try to relogin");
+      console.error(err);
+    }
+  });
+
+  const authLink = setContext((_request, { headers }) => {
+    const token = getAccessToken();
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `bearer ${token}` : ""
+      }
+    };
+  });
+
+  const errorLink = onError(({ graphQLErrors, networkError }) => {
+    console.log(graphQLErrors);
+    console.log(networkError);
+  });
   return new ApolloClient({
     ssrMode: typeof window === 'undefined',
-    link: new HttpLink({
-      uri: 'https://blog-server-v1.herokuapp.com/graphql', // Server URL (must be absolute)
-      credentials: 'include', // Additional fetch() options like `credentials` or `headers`
-    }),
+    link: ApolloLink.from([refreshLink, authLink, errorLink, httpLink] as any),
     cache: new InMemoryCache({
       typePolicies: {
         Query: {
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,8 +6,6 @@ import 'nprogress/nprogress.css'; //styles of nprogress
 import { useApollo } from 'src/lib/apollo';
 import { Provider } from 'react-redux';
 import store from 'src/store/store';
-import { AuthProvider } from 'src/component/common/Auth';
-
 //Binding events.
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
@@ -18,11 +16,9 @@ function App({ Component, pageProps }) {
 
   return (
     <Provider store={store}>
-      <AuthProvider>
-        <ApolloProvider client={apolloClient}>
-          <Component {...pageProps} />
-        </ApolloProvider>
-      </AuthProvider>
+      <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />
+      </ApolloProvider>
     </Provider>
   );
 }
